417982884b81e3a32524adbd477f691724b9089a
Felipe Frizzo <felipefrizzo@gmail.com>
2016-09-06 00:35:46 -0300

    Create Agency Class, Dao and Service

---
+++ src/main/java/br/univel/model/agency/Agency.java
@@ -0,0 +1,54 @@
+package br.univel.model.agency;
+
+import javax.persistence.*;
+
+/**
+ * Created by felipefrizzo on 9/5/16.
+ */
+@Entity
+@Table(name = "agency")
+public class Agency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "numero")
+    private String numero;
+    @Column(name = "cidade")
+    private String cidade;
+
+    public Agency() {
+    }
+
+    protected Agency(final String name, final String numero, final String cidade) {
+        this.name = name;
+        this.numero = numero;
+        this.cidade = cidade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
