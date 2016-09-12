4f49cc451a925fb812fd9a7856ecf17f2e746271
Felipe Frizzo <felipefrizzo@gmail.com>
2016-09-12 13:04:27 -0300

    Create Main and Cash Withdrawal Layout/Controller

---
+++ src/main/java/br/univel/view/cashwithdrawl/CashWithdrawalController.java
@@ -0,0 +1,43 @@
+package br.univel.view.cashwithdrawl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by felipefrizzo on 9/12/16.
+ */
+public class CashWithdrawalController {
+    @FXML
+    private TextField withdrawalValue;
+
+    @FXML
+    void handleConfirmWithdrawal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleWithdrawal50(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleWithdrawal100(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleWithdrawal200(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleWithdrawal300(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleWithdrawal500(ActionEvent event) {
+
+    }
+}
