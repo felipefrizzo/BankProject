88c8f45236f08166115e0d9609b3363b79d4e123
Felipe Frizzo <felipefrizzo@gmail.com>
2016-09-13 19:35:17 -0300

    Create Payment Layout and controller

---
+++ src/main/java/br/univel/view/payment/PaymentController.java
@@ -0,0 +1,21 @@
+package br.univel.view.payment;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by felipefrizzo on 9/13/16.
+ */
+public class PaymentController {
+    @FXML
+    private TextField barCode;
+
+    @FXML
+    private TextField valuePayment;
+
+    @FXML
+    void handlePaymentConfirm(ActionEvent event) {
+
+    }
+}
