c813c4f73cea65318a17ca88d6d85a30be013cd1
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-18 17:25:21 -0300

    Implementando a listagem de clientes

---
+++ src/main/java/br/univel/view/banking/listingCustomers/ListingCustomersController.java
@@ -0,0 +1,30 @@
+package br.univel.view.banking.listingCustomers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ListingCustomersController {
+
+	@FXML
+	private TableColumn<?, ?> number;
+
+	@FXML
+	private Button print;
+
+	@FXML
+	private TableColumn<?, ?> agency;
+
+	@FXML
+	private TableColumn<?, ?> balance;
+
+	@FXML
+	private TableColumn<?, ?> typeAccount;
+
+	@FXML
+	private TableColumn<?, ?> openedIn;
+
+	@FXML
+	private TableColumn<?, ?> customer;
+
+}
