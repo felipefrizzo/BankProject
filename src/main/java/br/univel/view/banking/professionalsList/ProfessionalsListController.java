692940c5c50249d6395685c8384f84151ede6c59
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-18 17:13:01 -0300

    Implementando a professionalsList e controller

---
+++ src/main/java/br/univel/view/banking/professionalsList/ProfessionalsListController.java
@@ -0,0 +1,20 @@
+package br.univel.view.banking.professionalsList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ProfessionalsListController {
+
+	   @FXML
+	    private Button addBanking;
+
+	    @FXML
+	    private TableColumn<?, ?> name;
+
+	    @FXML
+	    private Button editBanking;
+
+	    @FXML
+	    private TableColumn<?, ?> username;
+}
