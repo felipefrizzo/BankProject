e8410cb9b6cb7ca60da95e4407263bb48048b423
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-18 17:43:50 -0300

    Implementando o form de agencias

---
+++ src/main/java/br/univel/view/banking/agency/AgencyFormController.java
@@ -0,0 +1,21 @@
+package br.univel.view.banking.agency;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AgencyFormController {
+
+	@FXML
+	private Button confirm;
+
+	@FXML
+	private TextField number;
+
+	@FXML
+	private TextField city;
+
+	@FXML
+	private TextField name;
+
+}
