329c5e0fdf95780355bf7bc536ec75dafe3f3c14
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-18 16:18:52 -0300

    Implemetando o Controller da newAccount

--- src/main/java/br/univel/view/banking/newaccount/NewAccountController.java
+++ src/main/java/br/univel/view/banking/newaccount/NewAccountController.java
@@ -2,11 +2,15 @@ package br.univel.view.banking.newaccount;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class NewAccountController {
+
+	@FXML
+	private Button confirm;
 
 	@FXML
 	private TextField agency;
@@ -34,14 +38,15 @@ public class NewAccountController {
 
 	@FXML
 	void handle(ActionEvent event) {
-
+		isValid();
 	}
 
 	boolean isValid() {
 		String errorMessage;
-		if ((this.agency != null) && (this.name != null) && (this.typeAccount != null) && (this.username != null)
-				&& (this.passwordAccess != null) && (this.passwordOperation != null) && (this.cpf != null)
-				&& (this.age != null)) {
+		if ((this.agency.getText() == null) && (this.name.getText() == null) && (this.typeAccount == null)
+				&& (this.username.getText() == null) && (this.passwordAccess.getText() == null)
+				&& (this.passwordOperation.getText() == null) && (this.cpf.getText() == null)
+				&& (this.age.getText() == null)) {
 			int lenghtPassword = this.passwordOperation.getLength();
 			if (lenghtPassword == 6) {
 				return true;
