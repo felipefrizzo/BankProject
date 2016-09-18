d820d33e868bd0ac1e7eae4379632800a77e1648
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-18 16:58:31 -0300

    Implemetando a janela PopOut e o controller

---
+++ src/main/java/br/univel/view/banking/popoutConta/PopOutController.java
@@ -0,0 +1,30 @@
+package br.univel.view.banking.popoutConta;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class PopOutController {
+
+	@FXML
+	private Button confirm;
+
+	@FXML
+	private TextField agency;
+
+	@FXML
+	private ComboBox<?> typeAccount;
+
+	@FXML
+	private TextField holder;
+
+	@FXML
+	private TextField account;
+	
+	@FXML
+	private void handle(){
+		
+	}
+
+}
