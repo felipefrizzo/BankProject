2f6001c8c852a12bde77ac7210dc55335d165c0e
root <root@localhost.localdomain>
2016-09-23 22:07:17 -0300

    Iniciando o AgencyController

--- src/main/java/br/univel/view/banking/agency/AgencyController.java
+++ src/main/java/br/univel/view/banking/agency/AgencyController.java
@@ -1,22 +1,39 @@
 package br.univel.view.banking.agency;
 
+import br.univel.Main;
+import br.univel.model.agency.Agency;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AgencyController {
+	
+	private Main main;
+	private Agency agency = new Agency();
+	
+	public void setMain(Main main){
+		this.main = main;
+	}
+	
+	public void initialize(){
+		columnumber.setCellValueFactory(new PropertyValueFactory <> ("numero"));
+		columcity.setCellValueFactory(new PropertyValueFactory <> ("cidade"));
+		columname.setCellValueFactory(new PropertyValueFactory <> ("name"));
+	}
+	
 
 	@FXML
-	private TableColumn<?, ?> number;
+	private TableColumn<Agency, String> columnumber;
 
 	@FXML
-	private TableColumn<?, ?> city;
+	private TableColumn<Agency, String> columcity;
+
+	@FXML
+	private TableColumn<Agency, String> columname;
 
 	@FXML
 	private Button editAgency;
-
-	@FXML
-	private TableColumn<?, ?> name;
 
 	@FXML
 	private Button newAgency;
