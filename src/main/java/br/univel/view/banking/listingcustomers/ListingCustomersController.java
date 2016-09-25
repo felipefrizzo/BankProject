97f664e44a4ddf6f0b560d37b48dfd247a7118ba
lfsobrinho <luisfelipee.sob@gmail.com>
2016-09-25 15:10:33 -0300

    novo metodo

--- src/main/java/br/univel/view/banking/listingcustomers/ListingCustomersController.java
+++ src/main/java/br/univel/view/banking/listingcustomers/ListingCustomersController.java
@@ -3,37 +3,63 @@ package br.univel.view.banking.listingcustomers;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import br.univel.Main;
+import br.univel.model.account.Account;
 import br.univel.model.account.TypeAccount;
-import br.univel.model.person.Customer;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ListingCustomersController {
 
+	private Main main;
+	private Account account;
+	
+	private void setMain(Main main) {
+		this.main = main;
+	}
+
+	private void setAccount(Account account) {
+		this.account = account;
+	}
+
 	@FXML
-	private TableView<Customer> tableCustomers;
+	public void initialize(){
+		numberColumn.setCellValueFactory(new PropertyValueFactory<>("accountNumber"));
+		agencyColumn.setCellValueFactory(new PropertyValueFactory<>("agency"));
+		customerColumn.setCellValueFactory(new PropertyValueFactory<>("client"));
+		typeAccountColumn.setCellValueFactory(new PropertyValueFactory<>("typeAccount"));
+		balanceColumn.setCellValueFactory(new PropertyValueFactory<>("balance"));
+		openedInColumn.setCellValueFactory(new PropertyValueFactory<>("openingDate"));
+		
+	}
 	
 	@FXML
-	private TableColumn<Customer, String> number;
-
-	@FXML
-	private TableColumn<Customer, String> agency;
-
-	@FXML
-	private TableColumn<Customer, BigDecimal> balance;
-
-	@FXML
-	private TableColumn<Customer, TypeAccount> typeAccount;
-
-	@FXML
-	private TableColumn<Customer, Date> openedIn;
-
-	@FXML
-	private TableColumn<Customer, String> customer;
+	private TableView<Account> tableCustomers;
 	
 	@FXML
-	private Button print;
+	private TableColumn<Account, String> numberColumn;
+
+	@FXML
+	private TableColumn<Account, String> agencyColumn;
+
+	@FXML
+	private TableColumn<Account, BigDecimal> balanceColumn;
+
+	@FXML
+	private TableColumn<Account, TypeAccount> typeAccountColumn;
+
+	@FXML
+	private TableColumn<Account, Date> openedInColumn;
+
+	@FXML
+	private TableColumn<Account, String> customerColumn;
+	
+	@FXML
+	private void handlePrint(){
+		
+	}
+	
 
 }
